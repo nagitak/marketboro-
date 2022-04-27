e2b95d1c93d41118e6a466c07b8d92124a0f2738
GITAK NA <97418591+nagitak@users.noreply.github.com>
2022-04-27 23:44:11 +0900

    최종

--- app.js
+++ app.js
@@ -34,18 +34,6 @@ app.use(
     })
 );
 
-// express사용 정보 숨기기
-app.disable('x-powered-by');
-
-// xss(교차 사이트 스크립팅) 공격 방어
-app.use(helmet.xssFilter());
-
-// 클릭재킹으로 부터 보호
-app.use(helmet.frameguard('deny'));
-
-// 브라우저에서 파일 형식의 임의 추측 금지
-app.use(helmet.noSniff());
-
 app.listen(port, () => {
     console.log('서버가 연결되었습니다.');
 });
